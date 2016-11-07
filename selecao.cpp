90170dd3296dea4606891d4651ae5d1664f9a08a
Gabriel Torres <gabieltorres2000@gmail.com>
2016-11-07 13:53:17 -0200

    Removendo espaço desnecessário

--- selecao.cpp
+++ selecao.cpp
@@ -1,44 +1,20 @@
 #include "ordenacao.h"
 
-
 void Selecao(unsigned long *v, unsigned long n, unsigned long *mov, unsigned long *comp)
-
 {
-
 	int i, j, min;
-
-
-
 	unsigned long x;
 
-
-
-
-
-
-
    	for (i = 0; i < n-1; ++i)
-
    	{
-
       	min = i;
-
-
-
       	for (j = i+1; j < n; ++j)
         {
-
-
-
       		//v[j] < v[min]
       		(*comp)++;
-
          	if (v[j] < v[min])
-
          		min = j;
-
         }
-
      	//mov+3
      	(*mov)++;
       	x = v[i];
@@ -49,12 +25,5 @@ void Selecao(unsigned long *v, unsigned long n, unsigned long *mov, unsigned lon
      	(*mov)++;
       	v[min] = x;
 
-
-
    	}
-
-
-
 }
-
-
